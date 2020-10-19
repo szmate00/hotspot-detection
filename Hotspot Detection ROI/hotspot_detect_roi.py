f2836539e8f01e0ab484d4c16d1a74725d64f631
Szűcs Máté <56480262+szmate00@users.noreply.github.com>
2020-10-19 14:48:28 +0200

    Update hotspot_detect_roi.py

--- Hotspot Detection ROI/hotspot_detect_roi.py
+++ Hotspot Detection ROI/hotspot_detect_roi.py
@@ -3,139 +3,170 @@ import numpy as np
 import h5py
 from matplotlib import pyplot as plt
 import cv2
+import pandas as pd
 from scipy.signal import medfilt
 import os
+from PyPDF2 import PdfFileMerger
 plt.style.use('seaborn-whitegrid')
 
 
-File = 'AEQ31_edi_20181002_025_125045'
-threshold = 3600
+# list consisting the name of folders to be processed, each folder contains the given day's data files
+days = ['20181018']
 
-with h5py.File(f'C:/TDK/adatok/{File}.h5', 'r') as hdf:
-    roip = hdf.get('ROIP')
-    roip1 = roip.get('ROIP1')
-    roip1_data = roip1.get('ROIP1Data')
-    roip1_average = roip1.get('ROIP1Average')
-    
-    # calculating a decent median filter range for the given file
-    med_range = 2 * (roip1_data.shape[2]//3) + 1
-    roip1_median = medfilt(roip1_average, med_range)
+for day in days:
 
-    frames1 = []
-    List1 = []
-    List21 = []
+    threshold = 2800
+    Files = []
+    # censored path on github
+    path = ''
+    pdfs = []
+    merger = PdfFileMerger()
 
-    # ROI1
-    for i in range(roip1_data.shape[2]):
+    # searching path directory for the right files to analyse
+    for file in [f for f in os.listdir(f'/{path}') if not f.endswith(('down.h5', '.dat', '.tif', '.filepart'))]:
+        Files.append(file.replace('.h5', ''))
 
-        # condition to sort out faulty frames
-        if 1.3 * roip1_median[i] < roip1_average[i]:
+    Files = sorted(Files)
 
-            frames1.append(0)
-            List21.append(-1)
-            List1.append(-1)
+    # crrating folders for the outputs
+    os.makedirs(f'/home/szucsmate/roi/{path[-8:]}')
+    os.makedirs(f'/home/szucsmate/roi/{path[-8:]}/pdf')
+    os.makedirs(f'/home/szucsmate/roi/{path[-8:]}/csv')
 
-        else:
+    # going through each data file
+    for m in Files:
 
-            image1 = np.array(roip1_data[90:240, 465:570, i])
+        with h5py.File(f'/{path}/{m}.h5', 'r') as hdf:
+            roip = hdf.get('ROIP')
+            roip1 = roip.get('ROIP1')
+            roip1_data = roip1.get('ROIP1Data')
+            roi1_average = roip1.get('ROIP1Average')
 
-            # using median filter
-            blur1 = cv2.medianBlur(image1, 3)
+            # calculating the right size of median filter for each file
+            med_range = 2 * (roip1_data.shape[2]//3) + 1
+            roi1_medfilt = medfilt(roi1_average, med_range)
 
-            # binarization
-            thresh1 = cv2.threshold(blur1, threshold, 4095, cv2.THRESH_BINARY)[1]
+            frames1 = []
+            List1 = []
+            List21 = []
 
-            # counting the nonzero pixel values in the image
-            nzCount1 = cv2.countNonZero(thresh1)
+            # ROI1
+            for i in range(roip1_data.shape[2]):
 
-            # adding number of nonzero pixels to predifined list
-            frames1.append(nzCount1)
+                # sorting out faulty frames
+                if 1.3*roi1_medfilt[i] < roi1_average[i]:
 
-            # adding the number of the frames and binary value of hotspot to
-            if nzCount1 == 0:
-                List1.append('no')
-                List21.append(0)
-            else:
-                List1.append('yes')
-                List21.append(1)
+                    frames1.append(0)
+                    List21.append(-1)
+                    List1.append(-1)
 
-    frames2 = []
-    List2 = []
-    List22 = []
+                else:
 
-    # ROI2
-    for j in range(roip1_data.shape[2]):
+                    image1 = np.array(roip1_data[90:240, 465:570, i])
 
-        # condition to sort out faulty frames
-        if 1.3 * roip1_median[j] < roip1_average[j]:
+                    # using median filter
+                    blur1 = cv2.medianBlur(image1, 1)
 
-            frames2.append(0)
-            List22.append(-1)
-            List2.append(-1)
+                    # binarization
+                    thresh1 = cv2.threshold(blur1, threshold, 4095, cv2.THRESH_BINARY)[1]
 
-        else:
+                    # counting the nonzero pixel values in the image
+                    nzCount1 = cv2.countNonZero(thresh1)
 
-            image2 = np.array(roip1_data[65:265, 440:595, j])
+                    # adding number of nonzero pixels to predifined list
+                    frames1.append(nzCount1)
 
-            # using median filter
-            blur2 = cv2.medianBlur(image2, 3)
+                    # adding the number of the frames and binary value of hotspot to
+                    if nzCount1 == 0 or nzCount1/15750 > 0.15:
+                        List1.append(0)
+                        List21.append(0)
+                    else:
+                        List1.append(1)
+                        List21.append(1)
 
-            # binarization
-            thresh2 = cv2.threshold(blur2, threshold, 4095, cv2.THRESH_BINARY)[1]
+            frames2 = []
+            List2 = []
+            List22 = []
 
-            # counting the nonzero pixel values in the image
-            nzCount2 = cv2.countNonZero(thresh2)
+            # ROI2
+            for j in range(roip1_data.shape[2]):
 
-            # adding number of nonzero pixels to predifined list
-            frames2.append(nzCount2)
+                # sorting out faulty frames
+                if 1.3*roi1_medfilt[j] < roi1_average[j]:
 
-            # adding the number of the frames and binary value of hotspot to
-            if nzCount2 == 0:
-                List2.append('no')
-                List22.append(0)
-            else:
-                List2.append('yes')
-                List22.append(1)
+                    frames2.append(0)
+                    List22.append(-1)
+                    List2.append(-1)
 
-# creating a pdf with plots
-with PdfPages(f'{File}.pdf') as pdf:
-    plt.figure(figsize=(8.27, 11.69), dpi=100)
-    plt.suptitle(f'{File} (threshold={threshold})', fontweight="bold", fontsize=15)
-    plt.subplot(3, 1, 1)
-    plt.plot(frames1, color='r', label='size of ROI1: 85x130')
-    plt.title('ROI1', size=12)
-    plt.xlabel('Frames', size=12)
-    plt.ylabel('Number of pixels above threshold', size=12)
-    plt.legend(loc='upper right', fontsize=12, handlelength=0, handletextpad=0, frameon=True)
-    ax = plt.gca()
-    Ylim = ax.get_ylim()
+                else:
 
-    plt.subplot(3, 1, 2)
-    plt.plot(frames2, color='g', label='size of ROI2: 135x180')
-    plt.title('ROI2', size=12)
-    plt.xlabel('Frames', size=12)
-    plt.ylabel('Number of pixels above threshold', size=12)
-    plt.legend(loc='upper right', fontsize=12, handlelength=0, handletextpad=0, frameon=True)
-    ax = plt.gca()
-    ax.set_ylim(Ylim)
+                    image2 = np.array(roip1_data[65:265, 440:595, j])
 
-    plt.subplot(3, 1, 3)
-    plt.plot(List21, color='r', label='ROI1')
-    plt.plot(List22, color='g', label='ROI2')
+                    # using median filter
+                    blur2 = cv2.medianBlur(image2, 1)
 
-    plt.title('Binary Classification', size=12)
-    plt.xlabel('Frames', size=12)
-    plt.ylabel('Hotspot', size=12)
-    plt.legend(loc='upper right', fontsize=12, frameon=True)
+                    # binarization
+                    thresh2 = cv2.threshold(blur2, threshold, 4095, cv2.THRESH_BINARY)[1]
 
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+                    # counting the nonzero pixel values in the image
+                    nzCount2 = cv2.countNonZero(thresh2)
 
-    #pdf.savefig()
-    #plt.close()
-    plt.show()
+                    # adding number of nonzero pixels to predifined list
+                    frames2.append(nzCount2)
 
+                    # adding the number of the frames and binary value of hotspot to
+                    if nzCount2 == 0 or nzCount2/15750 > 0.15:
+                        List2.append(0)
+                        List22.append(0)
+                    else:
+                        List2.append(1)
+                        List22.append(1)
 
+        # creating a pdf with plots
+        with PdfPages(f'/home/szucsmate/roi/{path[-8:]}/pdf/{m}.pdf') as pdf:
+            plt.figure(figsize=(8.27, 11.69), dpi=100)
+            plt.suptitle(f'{m} (threshold={threshold})', fontweight="bold", fontsize=15)
+            plt.subplot(3, 1, 1)
+            plt.plot(frames1, color='r', label='size of ROI1: 85x130')
+            plt.title('ROI1', size=12)
+            plt.xlabel('Frames', size=12)
+            plt.ylabel('Number of pixels above threshold', size=12)
+            plt.legend(loc='upper right', fontsize=12, handlelength=0, handletextpad=0, frameon=True)
+            ax = plt.gca()
+            Ylim = ax.get_ylim()
 
+            plt.subplot(3, 1, 2)
+            plt.plot(frames2, color='g', label='size of ROI2: 135x180')
+            plt.title('ROI2', size=12)
+            plt.xlabel('Frames', size=12)
+            plt.ylabel('Number of pixels above threshold', size=12)
+            plt.legend(loc='upper right', fontsize=12, handlelength=0, handletextpad=0, frameon=True)
+            ax = plt.gca()
+            ax.set_ylim(Ylim)
 
+            plt.subplot(3, 1, 3)
+            plt.plot(List21, color='r', label='ROI1')
+            plt.plot(List22, color='g', label='ROI2')
 
+            plt.title('Binary Classification', size=12)
+            plt.xlabel('Frames', size=12)
+            plt.ylabel('Hotspot', size=12)
+            plt.legend(loc='upper right', fontsize=12, frameon=True)
 
+            plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+            pdf.savefig()
+            plt.close()
+
+            pdfs.append(f'/home/szucsmate/roi/{path[-8:]}/pdf/{m}.pdf')
+
+            # saving binary hotspot classification to .csv
+            df = pd.DataFrame(List1, columns=["Hotspot"])
+            df.to_csv(f'/home/szucsmate/roi/{path[-8:]}/csv/{m}.csv', index=True)
+
+    # creating a merged pdf containing all the the pdfs for the data files in the given folder
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    merger.write(f"/home/szucsmate/roi/{path[-8:]}/{path[-8:]}.pdf")
+    merger.close()
